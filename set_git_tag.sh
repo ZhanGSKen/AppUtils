e49fab3350fa77828f03be66a3a792b7b12fbcc6
ZhanGSKen <ZhanGSKen@QQ.COM>
2024-06-24 13:18:28 +0800

    编译配置修改

--- set_git_tag.sh
+++ set_git_tag.sh
@@ -11,14 +11,13 @@ git config --global --add safe.directory "${1}"
 echo "Current dir : "`pwd`
 
 ## 读取properties文件，更新属性
-properties_path="SetBuildFlagGitTag.properties"
+properties_path="buildf_flag.properties"
 ## 使用grep查找"^<key>="，然后使用awk提取值
 versionName=${2}
 stageCount=$(grep "^stageCount=" ${properties_path} | awk -F '=' '{print $2}')
-buildCount=$(grep "^buildCount=" ${properties_path} | awk -F '=' '{print $2}')
 
 ## 设新的Stage标签
-tag="v"${versionName}"."${stageCount:0}"."${buildCount:0}
+tag="v"${versionName}"."${stageCount:0}"
 
 ## 如果Git已经提交了所有代码就执行标签操作
 if [[ -n $(git diff --stat)  ]]
